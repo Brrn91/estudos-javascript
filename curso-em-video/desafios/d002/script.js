6509973f7339238099db36509c6388d1187a1c5f
Lucas Bruno <132234041+Brrn91@users.noreply.github.com>
2025-09-02 15:01:13 -0300

    Update imagens script.js

--- curso-em-video/desafios/d002/script.js
+++ curso-em-video/desafios/d002/script.js
@@ -15,28 +15,37 @@ function verificar() {
       genero = 'Homem';
       if (idade >= 0 && idade < 10) {
         //Criança
+        img.setAttribute('src', 'imagens/baby-boy.png');
       } else if (idade >= 10 && idade < 18) {
         //Jovem
+        img.setAttribute('src', 'imagens/jovem-homem.png');
       } else if (idade >= 18 && idade < 50) {
         //Adulto
+        img.setAttribute('src', 'imagens/adulto-homem.png');
       } else {
         idade >= 50 && idade < 100;
         //Idoso
+        img.setAttribute('src', 'imagens/idoso-homem.png');
       }
     } else if (fsex[1].checked) {
       genero = 'Mulher';
       if (idade >= 0 && idade < 10) {
         //Criança
+        img.setAttribute('src', 'imagens/baby-girl.png');
       } else if (idade >= 10 && idade < 18) {
         //Jovem
+        img.setAttribute('src', 'imagens/jovem-mulher.png');
       } else if (idade >= 18 && idade < 50) {
         //Adulto
+        img.setAttribute('src', 'imagens/adulta-mulher.png');
       } else {
         idade >= 50 && idade < 100;
         //Idoso
+        img.setAttribute('src', 'imagens/idosa-mulher.png');
       }
     }
-    res.style.textAlign = 'center';
+    res.style.textAlign = 'center'
     res.innerHTML = `Detectamos ${genero} com ${idade} anos.`;
+    res.appendChild(img);
   }
 }
