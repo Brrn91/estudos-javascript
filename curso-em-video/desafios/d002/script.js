212160ac89cbc15f4fe4bd07b170b98c8cf3c31e
Lucas Bruno <132234041+Brrn91@users.noreply.github.com>
2025-09-02 14:46:31 -0300

    idade

--- curso-em-video/desafios/d002/script.js
+++ curso-em-video/desafios/d002/script.js
@@ -1,13 +1,42 @@
-function verificar () {
-  var data = new Date()
-  var ano = data.getFullYear()
-  var fano = document.getElementById('txtano')
-  var res = document.querySelector('div#res')
+function verificar() {
+  var data = new Date();
+  var ano = data.getFullYear();
+  var fano = document.getElementById('txtano');
+  var res = document.querySelector('div#res');
   if (fano.value.length == 0 || Number(fano.value) > ano) {
-    alert('[ERRO] Verifique os dados e tente novamente!')
+    alert('[ERRO] Verifique os dados e tente novamente!');
   } else {
-  var fsex = document.getElementsByName('radsex')
-  var idade = ano - Number(fano.value)
-  
+    var fsex = document.getElementsByName('radsex');
+    var idade = ano - Number(fano.value);
+    var genero = ' ';
+    var img = document.createElement('img');
+    img.setAttribute('id', 'foto');
+    if (fsex[0].checked) {
+      genero = 'Homem';
+      if (idade >= 0 && idade < 10) {
+        //Criança
+      } else if (idade >= 10 && idade < 18) {
+        //Jovem
+      } else if (idade >= 18 && idade < 50) {
+        //Adulto
+      } else {
+        idade >= 50 && idade < 100;
+        //Idoso
+      }
+    } else if (fsex[1].checked) {
+      genero = 'Mulher';
+      if (idade >= 0 && idade < 10) {
+        //Criança
+      } else if (idade >= 10 && idade < 18) {
+        //Jovem
+      } else if (idade >= 18 && idade < 50) {
+        //Adulto
+      } else {
+        idade >= 50 && idade < 100;
+        //Idoso
+      }
+    }
+    res.style.textAlign = 'center';
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos.`;
   }
 }
