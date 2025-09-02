64edcc5b343ac3564d8d1aa5ba2ecdee3ef4d369
Lucas Bruno <132234041+Brrn91@users.noreply.github.com>
2025-09-02 13:58:41 -0300

    add functions

--- curso-em-video/desafios/d001/script.js
+++ curso-em-video/desafios/d001/script.js
@@ -1,1 +1,17 @@
-alert('Olá')
+function carregar() {
+  var msg = document.getElementById('msg');
+  var img = document.getElementById('imagem');
+  var data = new Date();
+  var hora = data.getHours();
+  msg.innerHTML = `Agora são ${hora} horas.`;
+  if (hora >= 0 && hora < 12) {
+    //BOM DIA!
+    img.src = 'imagens/manha.png'
+  } else if (hora >= 12 && hora <= 18) {
+    //BOA TARDE!
+    img.src = 'imagens/tarde.png'
+  } else {
+    //BOA NOITE!
+    img.src = 'imagens/noite.png'
+  }
+}
