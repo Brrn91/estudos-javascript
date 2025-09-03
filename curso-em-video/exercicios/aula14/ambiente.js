b463bdbb6fbadd675926944729a847cc6b253f57
Lucas Bruno <132234041+Brrn91@users.noreply.github.com>
2025-09-03 18:44:34 -0300

    for

---
+++ curso-em-video/exercicios/aula14/ambiente.js
@@ -0,0 +1,19 @@
+console.log('Vai começar...')
+for(var c = 1;c <= 4;c = c + 1) {
+  console.log(c)
+}
+console.log('Fim!')
+
+/* 
+var c = 1
+do {
+  console.log(c);
+  c++;
+} while (c <= 5);
+
+var c = 1
+while (c <= 5) {
+  console.log(c)
+  c = c + 1
+}
+  */
