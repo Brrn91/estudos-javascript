e6999a2e8d5ce55d5883834f727df5ffc3c7705e
Lucas Bruno <lucasbruno81@gmail.com>
2025-11-01 17:07:51 -0300

    variáveis em js

--- origamid/variaveis/script.js
+++ origamid/variaveis/script.js
@@ -1,1 +1,11 @@
-console.log("Oi")
+var nome = "Lucas";
+var idade = 28;
+var possuiFaculdade = true;
+
+console.log(nome, idade , possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
