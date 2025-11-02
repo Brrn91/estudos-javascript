5309c9347ee48a949f5b404f94f0eaccb13e3662
Lucas Bruno <lucasbruno81@gmail.com>
2025-11-02 11:07:29 -0300

    exercicio variaveis

--- origamid/variaveis/script.js
+++ origamid/variaveis/script.js
@@ -17,9 +17,11 @@ console.log(sobrenome, cidade);
 
 const nome = 'Lucas'; // Variável com o nome
 var idade = 28; // Variável com idade
-var comidaFavorita; // Variável com comida favorita sem valor 
-let diaFavorito; // Variável 1 - Sem valor
-var diaDoAniversario; // Variável 2 - Sem valor
-var nomeDaEsposa; // Variável 3 - Sem valor
-var animalDeEstimacao; // Variável 4 - Sem valor
-var cidadeQueMora; // Variável 5 - Sem valor
+var comidaFavorita; // Variável com comida favorita sem valor
+var diaFavorito,
+  diaDoAniversario,
+  nomeDaEsposa,
+  animalDeEstimacao,
+  cidadeQueMora; // Variáveis sem valor
+
+console.log(nome, idade, comidaFavorita);
