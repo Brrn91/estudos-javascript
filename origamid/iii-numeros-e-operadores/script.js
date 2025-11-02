4859a7520b6011a2717f9ee54cb4df465fb2196d
Lucas Bruno <lucasbruno81@gmail.com>
2025-11-02 14:11:25 -0300

    variáveis com operadores aritiméticos

---
+++ origamid/iii-numeros-e-operadores/script.js
@@ -0,0 +1,16 @@
+var idade = 28;
+console.log(idade);
+
+//Operadores aritiméticos
+
+var total1 = 10 + 5 + 10; // 25
+var divisao = 200 / 5; // 40
+var modulo = 3578941945 % 3; // 1
+console.log(modulo);
+
+// Operadores aritiméticos em strings
+
+var soma = '100' + 50; // 10050 (Concatena o a string ao número)
+var subtracao = '100' - 50; // 50 (A subtração converte a striing em número)
+var multiplicacao = '100' * '2' // 200 (A multiplicação converte a striing em número)
+var divisao = 'Comprei 10' / 2; // NaN (Não faz o resultado pois tem caractéres junto a string.)
