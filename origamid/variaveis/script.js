f8b48ed65a3999603cdd990bd3b872f392483639
Lucas Bruno <lucasbruno81@gmail.com>
2025-11-02 11:02:46 -0300

    Variáveis

--- origamid/variaveis/script.js
+++ origamid/variaveis/script.js
@@ -14,3 +14,12 @@ var sobrenome = 'Mafra',
   cidade = 'Natal';
 
 console.log(sobrenome, cidade);
+
+const nome = 'Lucas'; // Variável com o nome
+var idade = 28; // Variável com idade
+var comidaFavorita; // Variável com comida favorita sem valor 
+let diaFavorito; // Variável 1 - Sem valor
+var diaDoAniversario; // Variável 2 - Sem valor
+var nomeDaEsposa; // Variável 3 - Sem valor
+var animalDeEstimacao; // Variável 4 - Sem valor
+var cidadeQueMora; // Variável 5 - Sem valor
