44421da47d441e906acf1f2bebb9ea257a7fe51d
Lucas Bruno <lucasbruno81@gmail.com>
2025-11-02 14:37:34 -0300

    exercícios resolvidos

--- origamid/iii-numeros-e-operadores/script.js
+++ origamid/iii-numeros-e-operadores/script.js
@@ -45,12 +45,10 @@ console.log(soma2);
 
 // Incremente o número 5 e retorne o seu valor incrementado
 var incremento2 = 5;
-console.log(incremento2++)
-console.log(incremento2);
+console.log(++incremento2);
 
 // Como dividir o peso por 2?
-var numero = 80;
+var numero = +"80" / 2;
 var unidade = 'kg';
-var peso = +numero + unidade; // '80kg'
-var pesoPorDois = +peso / 2; // NaN (Not a Number)
-console.log(+pesoPorDois);
+var peso = numero + unidade; // '80kg'
+console.log(peso);
