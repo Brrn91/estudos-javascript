e9353903c1f9ceac87673029d033256954873378
Lucas Bruno <lucasbruno81@gmail.com>
2025-11-05 19:55:00 -0300

    boleanos

---
+++ origamid/iv-boolean/script.js
@@ -0,0 +1,21 @@
+// Boleanos
+var possuiGraduacao = true;
+
+if (possuiGraduacao) {
+  console.log('É verdadeiro!');
+} else {
+  console.log('É falso!');
+}
+
+// Condicionais else if
+
+var possuiGraduacao = true;
+var possuiDoutorado = true;
+
+if (possuiGraduacao) {
+  console.log('É verdadeiro!');
+} else if (possuiDoutorado) {
+  console.log('Possui doutorado!');
+} else {
+  console.log('Não possui nada!');
+}
