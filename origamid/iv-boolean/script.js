b3695eda5a4be08503bd522546ebf11a25e6f956
Lucas Bruno <lucasbruno81@gmail.com>
2025-11-08 14:02:38 -0300

    Boolean e Condicionais 2

--- origamid/iv-boolean/script.js
+++ origamid/iv-boolean/script.js
@@ -47,24 +47,33 @@ if (possuiGraduacao) {
 // if(!!'') // false
 
 // Operadores de comparação
-10 > 5; // true
-5 > 10; // false
-20 < 10; // false
-10 <= 10; // true
-10 >= 11; // false
+// 10 > 5; // true
+// 5 > 10; // false
+// 20 < 10; // false
+// 10 <= 10; // true
+// 10 >= 11; // false
 
-10 == '10'; // true
-10 == 10; // true
-10 === '10'; // false
-10 === 10 // true
-10 != 15 // true
-10 != '10' // false
-10 !== '10' // true
+// 10 == '10'; // true
+// 10 == 10; // true
+// 10 === '10'; // false
+// 10 === 10 // true
+// 10 != 15 // true
+// 10 != '10' // false
+// 10 !== '10' // true
 
-true && true; // true
-true && false; // false
-false && true; // false
-'Gato' && 'Cão'; // 'Cão'
-(5 - 5) && (5 + 5); // 0
-'Gato' && false; // false
-(5 >= 5) && (3 < 6); // true
+// true && true; // true
+// true && false; // false
+// false && true; // false
+// 'Gato' && 'Cão'; // 'Cão'
+// (5 - 5) && (5 + 5); // 0
+// 'Gato' && false; // false
+// (5 >= 5) && (3 < 6); // true
+
+// Operadores Lógicos ||
+// true || true; // true
+// true || false; // true
+// false || true; // true
+// 'Gato' || 'Cão'; // 'Gato'
+// (5 - 5) || (5 + 5); // 10
+// 'Gato' || false; // Gato
+// (5 >= 5) || (3 < 6); // true
