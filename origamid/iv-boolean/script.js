02821451cf7a0fb4baae40322ec052e6b98929b0
Lucas Bruno <lucasbruno81@gmail.com>
2025-11-08 13:55:51 -0300

    Boleanos e condicionais

--- origamid/iv-boolean/script.js
+++ origamid/iv-boolean/script.js
@@ -10,7 +10,7 @@ if (possuiGraduacao) {
 // Condicionais else if
 
 var possuiGraduacao = true;
-var possuiDoutorado = true;
+var possuiDoutorado = false;
 
 if (possuiGraduacao) {
   console.log('É verdadeiro!');
@@ -19,3 +19,52 @@ if (possuiGraduacao) {
 } else {
   console.log('Não possui nada!');
 }
+
+// Verdadeiro ou falso
+
+// Falso
+// if(false)
+// if(0) // ou -0
+// if(NaN)
+// if(null)
+// if(undefined)
+// if('') // ou "" ou ``
+
+// Verdadeiro
+// if(true)
+// if(1)
+// if(' ')
+// if('andre')
+// if(-5)
+// if({})
+
+// Operador Lógico de Negação !
+// if(!true) // false
+// if(!1) // false
+// if(!'') // true
+// if(!undefined) // true
+// if(!!' ') // true
+// if(!!'') // false
+
+// Operadores de comparação
+10 > 5; // true
+5 > 10; // false
+20 < 10; // false
+10 <= 10; // true
+10 >= 11; // false
+
+10 == '10'; // true
+10 == 10; // true
+10 === '10'; // false
+10 === 10 // true
+10 != 15 // true
+10 != '10' // false
+10 !== '10' // true
+
+true && true; // true
+true && false; // false
+false && true; // false
+'Gato' && 'Cão'; // 'Cão'
+(5 - 5) && (5 + 5); // 0
+'Gato' && false; // false
+(5 >= 5) && (3 < 6); // true
