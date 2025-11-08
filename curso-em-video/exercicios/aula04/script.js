c86c995d25c17a8e1c164068170717d97bdb4ffd
Lucas Bruno <lucasbruno81@gmail.com>
2025-11-08 12:58:08 -0300

    script.js

---
+++ curso-em-video/exercicios/aula04/script.js
@@ -0,0 +1,6 @@
+window.alert('Olá, Mundo!'); //Janela com botão ok
+window.alert('Minha primeira mensagem!'); //Janela com botão ok
+window.confirm('Está gostando de JavaScript?'); //Janela com botão ok ou cancelar
+window.prompt('Qual é seu nome?'); //Janela com espaço para texto
+window.prompt('Qual a sua idade?'); //Janela com espaço para texto
+window.prompt('De onde você é?'); //Janela com espaço para texto
