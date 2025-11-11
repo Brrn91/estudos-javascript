7fac42e32f033d40ce9e404ac0f08fa6d0ac0478
Lucas Bruno <lucasbruno81@gmail.com>
2025-11-11 20:06:14 -0300

    resolvendo exercícios

--- origamid/v-funcoes/script-exercicio.js
+++ origamid/v-funcoes/script-exercicio.js
@@ -1,15 +1,34 @@
 // Crie uma função para verificar se um valor é Truthy
+function isTruthy(valor) {
+  return !!valor;
+} 
 
 // Crie uma função matemática que retorne o perímetro de um quadrado
 // lembrando: perímetro é a soma dos quatro lados do quadrado
-
+function perimetroQuadrado(lado) {
+  return(lado * 4);
+}
 // Crie uma função que retorne o seu nome completo
 // ela deve possuir os parâmetros: nome e sobrenome
+function nomeCompleto(nome, sobrenome) {
+  return `${nome} ${sobrenome}`;
+}
 
 // Crie uma função que verifica se um número é par
+function IsEven(numero) {
+  var modulo = numero % 2;
+  if(modulo === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 // Crie uma função que retorne o tipo de
 // dado do argumento passado nela (typeof)
+function tipoDeDado(dados) {
+  return typeof dados;
+}
 
 // addEventListener é uma função nativa do JavaScript
 // o primeiro parâmetro é o evento que ocorre e o segundo o Callback
