f3cdfd2fb21d95263fa17111f6b888886731f214
Lucas Bruno <lucasbruno81@gmail.com>
2025-11-16 09:15:39 -0300

    Update ambiente.js

--- curso-em-video/exercicios/aula13/ambiente.js
+++ curso-em-video/exercicios/aula13/ambiente.js
@@ -4,13 +4,11 @@ do {
   c++; // C = C +1
 } while (c <= 6);
 
-
 /* var c = 1;
 while (c <= 6) {
   console.log(`Passo ${c}`);
   c++ // C = C +1
 }
-
 
 console.log('Tudo bem?')
 console.log('Tudo bem?')
