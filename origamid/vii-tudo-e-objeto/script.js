365aad0bcff08a94bb6055d4df58f7aa8deea5f5
Lucas Bruno <lucasbruno81@gmail.com>
2025-11-22 11:50:38 -0300

    função click

---
+++ origamid/vii-tudo-e-objeto/script.js
@@ -0,0 +1,10 @@
+var nome = 'Lucas';
+
+nomeMinusculo = nome.toLowerCase();
+
+console.log(nomeMinusculo)
+
+var btn = document.querySelector('.btn');
+btn.addEventListener('click', function(){
+  console.log('clicou!')
+})
