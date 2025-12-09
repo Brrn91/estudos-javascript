3cebf65c4c7ee1cf53e1442dda9e816fdc0dd8d9
Brrn91 <lucasbruno81@gmail.com>
2025-12-09 18:01:24 -0300

    media

--- curso-em-video/desafios/d005/script.js
+++ curso-em-video/desafios/d005/script.js
@@ -49,10 +49,12 @@ function finalizar() {
       if (valores[pos] < menor) menor = valores[pos];
     }
 
+    media = soma / total;
     res.innerHTML = '';
     res.innerHTML += `<p>Ao todo, temos ${total} números cadastrados.</p>`;
     res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`;
     res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`;
     res.innerHTML += `<p>A soma dos valore é ${soma}.</p>`;
+    res.innerHTML += `<p>A média dos valores é ${media}.</p>`;
   }
 }
