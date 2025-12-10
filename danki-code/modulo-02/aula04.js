f6eed380f34fd088a1c1fbb6260b2ddce1b015e1
Brrn91 <lucasbruno81@gmail.com>
2025-12-10 20:57:39 -0300

    Condições

---
+++ danki-code/modulo-02/aula04.js
@@ -0,0 +1,28 @@
+//Condições
+
+var numero = 10;
+var nome = 'Lucas';
+
+// if(nome == 'Bruno'){
+//   console.log('Verdadeiro');
+// } else {
+//   console.log('Entrou no else: Falso');
+// }
+
+// if (numero === 10) {
+//   console.log('O numero é igual a 10');
+// } else if (numero < 10) {
+//   console.log('O numero é menor que 10');
+// } else if (numero > 10) {
+//   console.log('O numero é maior que 10');
+// }
+
+// if(nome != 'Lucas') {
+//   console.log('O nome não é Lucas');
+// } else if (nome === 'Lucas') {
+//   console.log('O nome é Lucas');
+// }
+
+if (numero == 10 || nome == 'Lucas') {
+  console.log('Verdadeiro');
+}
