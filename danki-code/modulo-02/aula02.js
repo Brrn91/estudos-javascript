e508c631a6048f5cd6c8038db3525e76c029ffdb
Brrn91 <lucasbruno81@gmail.com>
2025-12-10 18:04:39 -0300

    variáveis

---
+++ danki-code/modulo-02/aula02.js
@@ -0,0 +1,24 @@
+// var = variavel global
+// let = variavel no escopo
+// const = constante (não muda o valor)
+
+// var nome = 'Lucas';
+// nome = 'Bruno';
+// console.log(nome);
+
+// {
+//   let idade = 28;
+//   console.log(idade, nome);
+// }
+
+// const nome = 'Lucas';
+// console.log(nome);
+// nome = 'Bruno'; // ERRO
+
+var numero = 28;
+var nome = 'Lucas';
+var nome2 = 'Bruno';
+var bolean = true;
+var nula = null;
+
+console.log(`O nome é ${nome} e a idade é ${numero}.`)
