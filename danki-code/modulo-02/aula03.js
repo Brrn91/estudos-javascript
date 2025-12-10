0206b7615018c20eda1afa498ce0972d826e1fdf
Brrn91 <lucasbruno81@gmail.com>
2025-12-10 20:43:20 -0300

    Arrays

---
+++ danki-code/modulo-02/aula03.js
@@ -0,0 +1,15 @@
+// Arrays
+
+var nome1 = 'João';
+var nome2 = 'Maria';
+var nome3 = 'Pedro';
+
+// Utilizando array para armazenar múltiplos valores
+var nomes = ['João', 'Maria', 'Pedro'];
+// var nomes2 = new Array('João', 'Maria', 'Pedro'); Outra forma de criar um array
+
+nomes[0] = 'Ana'; // Alterando o primeiro elemento do Array
+
+nomes.push('Carla'); // Adicionando um novo elemento ao final do array
+
+console.log(nomes[3]); // Acessando o primeiro elemento do array
