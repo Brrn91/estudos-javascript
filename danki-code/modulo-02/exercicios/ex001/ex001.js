77b2abb397c87a6fa127eb8ec4c0b4d181ac81bf
Brrn91 <lucasbruno81@gmail.com>
2025-12-11 14:28:05 -0300

    ex001 - prompt

---
+++ danki-code/modulo-02/exercicios/ex001/ex001.js
@@ -0,0 +1,5 @@
+var nome = prompt('Qual o seu nome?');
+var idade = prompt('Qual a sua idade?');
+var cidade = prompt('Onde você mora?');
+
+console.log('Olá ' +nome, 'você tem ' +idade, 'anos e mora em ' +cidade);
