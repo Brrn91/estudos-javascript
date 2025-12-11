4a2b588d64b189f555808ca4e0b6f0841546dccb
Brrn91 <lucasbruno81@gmail.com>
2025-12-11 13:30:22 -0300

    funções

--- danki-code/modulo-02/aula05-funcoes.js
+++ danki-code/modulo-02/aula05-funcoes.js
@@ -1,35 +1,49 @@
-// function testes() {
-//   // Executado quando chamar a função
-//   console.log('Chamando a função testes');
-// }
+/*
+function testes() {
+  // Executado quando chamar a função
+  console.log('Chamando a função testes');
+}
+*/
 
-// function teste2(nome, idade) {
-//   console.log('O nome passado é: ' +nome);
-//   console.log('A idade passada é:' +idade);
-// }
+/*
+function teste2(nome, idade) {
+  console.log('O nome passado é: ' +nome);
+  console.log('A idade passada é:' +idade);
+}
 
-// testes(); // Chamando a função.
-// teste2('Lucas', 28)
+testes(); // Chamando a função.
+teste2('Lucas', 28)
 
-// function pegarNome() {
-//   return 'Lucas';
-// }
+function pegarNome() {
+  return 'Lucas';
+}
 
-// var nome = pegarNome();
-// console.log(nome);
+var nome = pegarNome();
+console.log(nome);
+*/
 
-// function pegarNome(par){
-//   if(par ==1){
-//     return 'Lucas';
-//   } else {
-//     return 'Outro nome';
-//   }
-// }
+/*
+function pegarNome(par){
+  if(par ==1){
+    return 'Lucas';
+  } else {
+    return 'Outro nome';
+  }
+}
+*/
 
-// var nome = pegarNome(1);
-// console.log(nome);
+/*
+var nome = pegarNome(1);
+console.log(nome);
+*/
 
+/*
 var func = function() {
   alert('Olá mundo!');
 }
 func();
+*/
+
+(function(){
+  alert('Olá mundo!');
+})();
