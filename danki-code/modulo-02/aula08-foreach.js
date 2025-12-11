9d1d23c118db6e407f6bba7abc871ef39c614975
Brrn91 <lucasbruno81@gmail.com>
2025-12-11 14:12:51 -0300

    forEach

---
+++ danki-code/modulo-02/aula08-foreach.js
@@ -0,0 +1,11 @@
+/*
+var arr = ['Lucas', 'Bruno', 'Mafra'];
+for (var i = 0; i < arr.length; i++){
+  console.log(arr[i]);
+}
+  */
+
+var arr = ['Lucas', 'Bruno', 'Mafra'];
+arr.forEach(function(value,index){
+  console.log(value);
+});
