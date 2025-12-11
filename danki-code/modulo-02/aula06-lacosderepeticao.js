c5c0059c41be5b3d1edbc5207c723dc28fccb49e
Brrn91 <lucasbruno81@gmail.com>
2025-12-11 13:47:52 -0300

    Laços de repetição

---
+++ danki-code/modulo-02/aula06-lacosderepeticao.js
@@ -0,0 +1,24 @@
+// Laços de repetição
+
+/*
+var n = 0;
+while (n < 10) {
+  // Execute aqui!
+  console.log(n);
+  n++;
+}
+  */
+
+/*
+for(var i = 0; i <10; i++) {
+  console.log(i);
+}
+*/
+
+/*
+var n = 0;
+do {
+  console.log(n);
+  n++;
+} while(n < 10);
+ */
