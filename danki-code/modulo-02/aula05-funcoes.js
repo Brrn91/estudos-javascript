d1df7354067b24380d5871a9bf3ac8761d8856b1
Brrn91 <lucasbruno81@gmail.com>
2025-12-10 21:17:53 -0300

    Funções

---
+++ danki-code/modulo-02/aula05-funcoes.js
@@ -0,0 +1,30 @@
+// function testes() {
+//   // Executado quando chamar a função
+//   console.log('Chamando a função testes');
+// }
+
+// function teste2(nome, idade) {
+//   console.log('O nome passado é: ' +nome);
+//   console.log('A idade passada é:' +idade);
+// }
+
+// testes(); // Chamando a função.
+// teste2('Lucas', 28)
+
+// function pegarNome() {
+//   return 'Lucas';
+// }
+
+// var nome = pegarNome();
+// console.log(nome);
+
+function pegarNome(par){
+  if(par ==1){
+    return 'Lucas';
+  } else {
+    return 'Outro nome';
+  }
+}
+
+var nome = pegarNome(1);
+console.log(nome);
