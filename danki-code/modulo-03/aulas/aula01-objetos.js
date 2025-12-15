d3b97570161db06d102e49bc6b904f4d0ec2d820
Brrn91 <lucasbruno81@gmail.com>
2025-12-15 16:43:49 -0300

    Create aula01-objetos.js

---
+++ danki-code/modulo-03/aulas/aula01-objetos.js
@@ -0,0 +1,22 @@
+/* var objetos = {
+  Nome: 'Lucas',
+  Idade: 28,
+  Profissão: 'Designer',
+};
+
+console.log(objetos.Nome); // Acessando a propriedade 'Nome' em um objeto */
+
+var pessoas = [
+  {
+    Nome: 'Lucas',
+    Idade: 28,
+    Profissão: 'Designer',
+  },
+  {
+    Nome: 'Bruno',
+    Idade: 28,
+    Profissão: 'Programador',
+  },
+];
+
+console.log(pessoas[1].Nome); // Acessando a propriedade 'Nome' do primeiro objeto no array 'Pessoas'
