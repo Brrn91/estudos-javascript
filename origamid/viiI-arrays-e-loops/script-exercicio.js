3aea56861b426a452f2b6d851564c8cd2021fe9a
Brrn91 <lucasbruno81@gmail.com>
2025-12-18 17:40:44 -0300

    finish

--- origamid/viiI-arrays-e-loops/script-exercicio.js
+++ origamid/viiI-arrays-e-loops/script-exercicio.js
@@ -1,11 +1,27 @@
 // Crie uma array com os anos que o Brasil ganhou a copa
 // 1959, 1962, 1970, 1994, 2002
 
+var campeaoCopa = ['1959', '1962', '1970', '1994', '2002'];
+
 // Interaja com a array utilizando um loop, para mostrar
 // no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
+
+for (var i = 0; i < campeaoCopa.length; i++) {
+  console.log(`O Brasil ganhou a copa de ${campeaoCopa[i]}`);
+}
 
 // Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
 var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância'];
 
+for (var item = 0; item < frutas.length; item++) {
+  console.log(frutas[item]);
+  if (frutas[item] === 'Pera') {
+    break;
+  }
+}
+
 // Coloque a última fruta da array acima em uma variável,
 // sem remover a mesma da array.
+
+var ultimaFruta = frutas[frutas.length - 1];
+console.log(ultimaFruta);
