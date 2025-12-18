3885ef75f58182b9224dae4172577c347466b624
Brrn91 <lucasbruno81@gmail.com>
2025-12-18 17:07:25 -0300

    break

---
+++ origamid/viiI-arrays-e-loops/script.js
@@ -0,0 +1,35 @@
+//ARRAYS - MÉTODOS DE ADIÇÃO E REMOÇÃO
+/*
+var videoGames = ['Switch', 'PS4', 'XBOX'];
+
+videoGames.pop(); //Remove o último item do array
+
+console.log(videoGames);
+videoGames.push('3DS'); //Adiciona um item no final do array
+
+console.log(videoGames);
+videoGames.length; //Mostra o tamanho do array
+
+console.log(videoGames.length);
+*/
+// LOOPS - LAÇOS DE REPETIÇÃO
+
+for (var numero = 0; numero <= 10; numero++){
+  console.log(numero);
+}
+
+var i = 0;
+while(i <=10) {
+  console.log(i);
+  i++;
+}
+
+var videoGames = ['Switch', 'PS4', 'XBOX', '3DS'];
+
+for(var item = 0; item < videoGames.length; item++) {
+  console.log(videoGames[item]);
+  if(videoGames[item] === 'PS4') {
+    break;
+  }
+}
+
