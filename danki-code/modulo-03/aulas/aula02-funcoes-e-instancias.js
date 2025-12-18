b026aaf09572bfd492dec6f8291d139e48b743ae
Brrn91 <lucasbruno81@gmail.com>
2025-12-18 13:31:18 -0300

    interagindo com objetos

--- danki-code/modulo-03/aulas/aula02-funcoes-e-instancias.js
+++ danki-code/modulo-03/aulas/aula02-funcoes-e-instancias.js
@@ -1,3 +1,4 @@
+/*
 function Pessoa(){
   this.nome = 'Lucas';
 }
@@ -8,3 +9,27 @@ var pessoa2 = new Pessoa();
 pessoa.nome = 'Bruno';
 
 console.log(pessoa.nome);
+*/
+
+function Pessoa(){
+  this.nome = 'Lucas';
+  this.idade = 28;
+  this.printNome = function(){
+    console.log(this.nome);
+  };
+  this.returnAnimal = function(animal){
+    return animal;
+  }
+}
+
+function Animal(){
+  this.nome = 'Cachorro';
+  this.raca = 'Salsicha';
+}
+
+var pessoa = new Pessoa();
+var animal = new Animal();
+
+animal.raca = 'Labrador';
+
+console.log(pessoa.returnAnimal(animal).raca);
