eb10362581f92692482175f28e5061639eaf5203
Brrn91 <lucasbruno81@gmail.com>
2026-01-16 21:00:20 -0300

    dom exercícios

--- origamid/k-dom/script.js
+++ origamid/k-dom/script.js
@@ -3,3 +3,8 @@
 // window.alert('Olá, mundo!')
 
 const h1Selecionado = document.querySelector('h1');
+const h1Classes = h1Selecionado.classList;
+
+h1Selecionado.addEventListener('click', function () {
+  console.log('Clicou em', h1Selecionado.innerText);
+});
