e1ff1c1c888773c43717f4dee750a8596a422f7d
Brrn91 <lucasbruno81@gmail.com>
2026-01-20 15:20:23 -0300

    exercicios

---
+++ origamid/l-projeto/selecionando-o-dom/script-exercicios.js
@@ -0,0 +1,14 @@
+// Retorne no console todas as imagens do site
+const todasAsImg = document.querySelectorAll('img');
+console.log(todasAsImg);
+
+// Retorne no console apenas as imagens que começaram com a palavra imagem
+
+const palavraImagem = document.querySelectorAll('[src^="img/imagem2"]');
+console.log(palavraImagem);
+
+// Selecione todos os links internos (onde o href começa com #)
+
+// Selecione o primeiro h2 dentro de .animais-descricao
+
+// Selecione o último p do site
