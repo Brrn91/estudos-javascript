090272f9dd90bf7a25665a67f5dead96d6c01de9
Brrn91 <lucasbruno81@gmail.com>
2026-01-23 16:00:15 -0300

    EXERCÍCIO

--- origamid/l-projeto/selecionando-o-dom/script-exercicios.js
+++ origamid/l-projeto/selecionando-o-dom/script-exercicios.js
@@ -4,11 +4,18 @@ console.log(todasAsImg);
 
 // Retorne no console apenas as imagens que começaram com a palavra imagem
 
-const palavraImagem = document.querySelectorAll('[src^="img/imagem2"]');
+const palavraImagem = document.querySelectorAll('img[src^="img/imagem"]');
 console.log(palavraImagem);
 
 // Selecione todos os links internos (onde o href começa com #)
 
+const linkInterno = document.querySelectorAll('[href^="#"]');
+console.log(linkInterno);
+
 // Selecione o primeiro h2 dentro de .animais-descricao
 
+const h2Animais = document.querySelector('.animais-descricao h2');
+console.log(h2Animais);
+
 // Selecione o último p do site
+
