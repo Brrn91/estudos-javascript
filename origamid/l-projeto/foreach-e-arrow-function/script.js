8bfd98027d7f3b7a01c90f814b0ab7bef8dee2ec
Brrn91 <lucasbruno81@gmail.com>
2026-01-25 10:56:01 -0300

    Update script.js

--- origamid/l-projeto/foreach-e-arrow-function/script.js
+++ origamid/l-projeto/foreach-e-arrow-function/script.js
@@ -1,6 +1,8 @@
 const imgs = document.querySelectorAll('img');
 
-imgs.forEach((item, index) => {
-  console.log(item, index);
+let i = 0
+imgs.forEach(function() {
+  console.log(i++)
 });
 
+imgs.forEach(() => console.log(i++));
