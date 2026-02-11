ce256fd77d3833cb64ccf641d7d032b82554005c
Brrn91 <lucasbruno81@gmail.com>
2026-02-11 15:04:01 -0300

    matchMedia

--- origamid/l-projeto/e-dimensoes-e-distancias/script.js
+++ origamid/l-projeto/e-dimensoes-e-distancias/script.js
@@ -7,9 +7,13 @@ console.log(height);
 const primeiroH2 = document.querySelector('h2');
 const h2Left = primeiroH2.offsetLeft;
 
-const rect = primeiroH2.getBoundingClientRect();
+const h2rect = primeiroH2.getBoundingClientRect();
 
-console.log(rect.top);
+console.log(h2rect.top);
+
+if (h2rect.top < 0) {
+  console.log( 'Passou do elemento!')
+}
 
 console.log(
   window.innerWidth,
@@ -18,3 +22,11 @@ console.log(
   window.outerHeight,
   window.pageYOffset,
 );
+
+const small = window.matchMedia( '(max-width: 600px');
+
+if(small.matches) {
+  console.log('Usuário mobile');
+} else {
+  console.log('Usuário desktop');
+}
