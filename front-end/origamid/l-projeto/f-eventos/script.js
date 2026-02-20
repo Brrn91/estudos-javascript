4bcb8036b2460cd5169c9e774f89771017286182
Brrn91 <lucasbruno81@gmail.com>
2026-02-20 20:56:12 -0300

    Update script.js

--- front-end/origamid/l-projeto/f-eventos/script.js
+++ front-end/origamid/l-projeto/f-eventos/script.js
@@ -1,9 +1,19 @@
 const img = document.querySelector('img');
 
-function callback() {
-  console.log('clicou');
+function callback(event) {
+  console.log(event);
 }
 
-img.addEventListener('click', callback);
+// img.addEventListener('click', callback);
 
 console.log(img);
+
+const animaisLista = document.querySelector('.animais-lista');
+
+function callbackLista(event) {
+  console.log(event.currentTarget)
+  console.log(event.target);
+};
+
+animaisLista.addEventListener('click', callbackLista);
+console.log(animaisLista);
