6bd695df50e9cc21ff1fbb3f06b58a7cdd5b4cc4
Brrn91 <lucasbruno81@gmail.com>
2026-02-20 21:01:25 -0300

    preventDefault

--- front-end/origamid/l-projeto/f-eventos/script.js
+++ front-end/origamid/l-projeto/f-eventos/script.js
@@ -11,9 +11,19 @@ console.log(img);
 const animaisLista = document.querySelector('.animais-lista');
 
 function callbackLista(event) {
-  console.log(event.currentTarget)
+  console.log(event.currentTarget);
   console.log(event.target);
-};
+  console.log(event.type);
+}
 
 animaisLista.addEventListener('click', callbackLista);
 console.log(animaisLista);
+
+const linkExterno = document.querySelector('a[href^=http]');
+
+function handleLinkExterno(event) {
+  event.preventDefault();
+  console.log(event);
+};
+
+linkExterno.addEventListener('click', handleLinkExterno);
